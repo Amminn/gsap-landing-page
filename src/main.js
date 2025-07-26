f8a40046465eef09326e4eb485ce55dd26d51735
Amin <55767303+Amminn@users.noreply.github.com>
2025-07-26 21:32:55 +0100

    feat: add vertical animation logic and it's and improve css and text

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-console.log("test");
+import { gsap } from "gsap";
+
+const ySlider = document.querySelector(".y-slider");
+const sliderSpanHeight = ySlider.querySelector("span").offsetHeight;
+
+function animateSlider() {
+  gsap.to(ySlider, {
+    y: -sliderSpanHeight,
+    duration: 1.1,
+    ease: "power2.inOut",
+    delay: 0.3,
+    onComplete: () => {
+      const firstSpan = ySlider.querySelector("span");
+      ySlider.appendChild(firstSpan);
+      gsap.set(ySlider, { y: 0 });
+      animateSlider();
+    },
+  });
+}
+
+animateSlider();
